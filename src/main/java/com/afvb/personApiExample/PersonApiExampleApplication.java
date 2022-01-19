e6c2cc4d18121ee6ce1889bd6301537461d3e5fa
vinicius de castro <vinicius2011castro@hotmail.com>
2022-01-18 23:21:30 -0300

    Primeiro Commit

---
+++ src/main/java/com/afvb/personApiExample/PersonApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.afvb.personApiExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiExampleApplication.class, args);
+	}
+
+}
